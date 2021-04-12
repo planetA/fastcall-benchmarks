4ca52c9dc666bbdd173c2b12a6d81c75430f4c0b
vilaureu <vilaureu@users.noreply.github.com>
2021-04-12 18:59:27 +0200

    fastcall-examples stack benchmark

--- main.cc
+++ main.cc
@@ -10,6 +10,7 @@
 #define NR_FASTCALL_SYSCALL 442
 #define FCE_DEVICE_FILE "/dev/fastcall-examples"
 #define IOCTL_EXAMPLE_NOOP 0
+#define IOCTL_EXAMPLE_STACK 1
 
 /*
  * Benchmark the execution of an empty system call by using sys_ni_syscall,
@@ -23,18 +24,20 @@ BENCHMARK(syscall_sys_ni_syscall);
 
 template <const int example> class ExamplesFixture : public benchmark::Fixture {
 public:
-  void SetUp(const ::benchmark::State &state) {
+  void SetUp(::benchmark::State &state) override {
     fd = open(FCE_DEVICE_FILE, O_RDONLY);
     if (fd < 0) {
-      throw "Failed to open device driver!";
+      state.SkipWithError("Failed to open device driver!");
+      return;
     }
     fc_index = ioctl(fd, example);
     if (fc_index < 0) {
-      throw "Failed to open device driver!";
+      state.SkipWithError("ioctl failed!");
+      return;
     }
   }
 
-  void TearDown(const ::benchmark::State &state) {
+  void TearDown(::benchmark::State &) override {
     close(fd);
     // TODO unregistering
   }
@@ -64,10 +67,25 @@ BENCHMARK(fastcall_noop);
 /*
  * Benchmark the noop fastcall function of fastcall-examples.
  */
-BENCHMARK_TEMPLATE_F(ExamplesFixture, fastcall_example_noop, IOCTL_EXAMPLE_NOOP)
+BENCHMARK_TEMPLATE_F(ExamplesFixture, fastcall_examples_noop,
+                     IOCTL_EXAMPLE_NOOP)
 (benchmark::State &state) {
   if (syscall(NR_FASTCALL_SYSCALL, fc_index) != 0) {
     state.SkipWithError("fastcall-examples noop failed!");
+    return;
+  }
+  for (auto _ : state)
+    syscall(NR_FASTCALL_SYSCALL, fc_index);
+}
+
+/*
+ * Benchmark the stack fastcall function of fastcall-examples.
+ */
+BENCHMARK_TEMPLATE_F(ExamplesFixture, fastcall_examples_stack,
+                     IOCTL_EXAMPLE_STACK)
+(benchmark::State &state) {
+  if (syscall(NR_FASTCALL_SYSCALL, fc_index) != 0) {
+    state.SkipWithError("fastcall-examples stack failed!");
     return;
   }
   for (auto _ : state)
